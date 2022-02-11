bc24da79734bbcd656fd118921daec7abd03c1f7
KovacsDora <szt2.gp01.20g@szlgbp.hu>
2022-02-11 09:09:24 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LogoKaresz
+{
+	public partial class Form1 : Form
+	{
+		/* Függvények */
+		void Bevezető_fa(int év,double meret)
+        {
+            if (év==1)
+            {
+				Előre(meret);
+				Jobbra(180);
+				Előre(meret);
+				Jobbra(180);
+            }
+            else
+            {
+				Előre(meret);
+				Balra(60);
+				Bevezető_fa(év - 1, meret * 0.5);
+				Jobbra(120);
+				Bevezető_fa(év - 1, meret * 0.5);
+
+
+				Jobbra(120);
+
+
+				Előre(meret);
+				Balra(180);
+
+            }
+        }
+
+		/* Függvények vége */
+		void FELADAT()
+		{
+			/* Ezt indítja a START gomb! */
+			Bevezető_fa(5, 100);
+			// Teleport(közép.X, közép.Y+150, észak);
+		}
+	}
+}
